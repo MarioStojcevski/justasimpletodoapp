787ccbff43103093ed03f988733283863d055641
Mario Stojcevski <mane.sto@outlook.com>
2026-04-19 21:25:55 +0200

    changed to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import { WorkspaceProvider } from './state/workspace'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <WorkspaceProvider>
         <App />
       </WorkspaceProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
